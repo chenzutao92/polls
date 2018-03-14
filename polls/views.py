b1aa15a43a199f7cfbaecf12d2ea93dda1abf003
chenzutao92 <534213375@qq.com>
2018-03-15 01:04:05 +0800

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # context_object_name指定上下文的名字
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        '''
+        返回最近发布的五个问题（不包括那些设置为
+     将来发布）。
+        '''
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        '''
+        排除尚未发布的任何问题。
+        '''
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# 投票
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, ChildProcessError):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功处理后始终返回HttpResponseRedirect
+        # 与POST数据。 这可以防止数据发布两次，如果一个
+        # 用户点击后退按钮。
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# def index(request):
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # # output = ','.join([q.question_text for q in latest_question_list])
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+# # 详情
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     question = get_object_or_404(Question.objects.get(pk=question_id))
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# # 结果
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+
+
